f4e4a4d7a6101bcd49236fb7a94793bbc458762e
Ralf Meermeier <ralf.meermeier@gmail.com>
2020-09-04 11:49:23 -0400

    Load in Python

---
+++ run.py
@@ -0,0 +1,53 @@
+import sys
+import godec
+import numpy as np
+import gc
+import subprocess
+
+def runme():
+  ovs = godec.overrides()
+
+  sample_rate = 48000
+  ovs.add("global_opts.SAMPLE_RATE",str(sample_rate))
+  capture_chunk_size = 64
+  ovs.add("global_opts.CAPTURE_CHUNK_SIZE",str(capture_chunk_size))
+  energy_window_size = 1024
+  energy_window_size_ms = 1000.0*energy_window_size/sample_rate
+  ovs.add("global_opts.ENERGY_WINDOW_SIZE_MS",str(energy_window_size_ms))
+  print("Energy window size "+str(energy_window_size)+"ms")
+  energy_window_step_factor = 4
+  energy_window_step_size_ms = energy_window_size_ms/energy_window_step_factor
+  ovs.add("global_opts.ENERGY_WINDOW_STEP_SIZE_MS",str(energy_window_step_size_ms))
+  print("Energy window step size "+str(energy_window_step_size_ms)+"ms")
+  
+  mic_audio_recording=False
+  if (not mic_audio_recording):
+    ovs.add("audio_writer_allchan","#audio_writer_allchan")
+  
+  rec_devname = subprocess.Popen('arecord -L | grep -B 1 "seeed-4mic-voicecard" | grep "^hw"', shell=True, stdout = subprocess.PIPE).stdout.read().decode().strip()
+  rec_devidx = subprocess.Popen('cat /proc/asound/cards | grep "seeed-4mic-voicecard" | head -1 | awk "{print $1;}"',shell=True, stdout = subprocess.PIPE).stdout.read().decode().strip()
+  ovs.add("4mic.soundcard_identifier",rec_devname)
+  
+  push_endpoints = godec.push_endpoints()
+  push_endpoints.add("soundcard_control")
+  
+  pull_endpoints = godec.pull_endpoints()
+  #pull_endpoints.add("pull_test", ["preproc_audio", "energy"])
+  
+  godec.load_godec("online.json", ovs, push_endpoints, pull_endpoints, True)
+  
+#  num_samples = 16000000
+#  data = np.random.randn(num_samples).astype('c')
+#  binary_message = godec.binary_decoder_message(num_samples-1, data, "sample_rate=16000")
+#  godec.push_message("raw_audio", binary_message)
+#  
+#  conv_message = godec.conversation_state_decoder_message(num_samples-1, "utt_id", True, "convo_id", True)
+#  godec.push_message("convstate", conv_message)
+#  
+#  msg_block = godec.pull_message("pull_test", 1000)
+#  print("new sample rate: "+str(msg_block["preproc_audio"].sample_rate))
+#  
+  godec.blocking_shutdown()
+  
+runme()
+
--- run.sh
+++ run.sh
@@ -9,41 +9,4 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/boost_1_74_0/stage/lib
 
 . ./config.sh
 
-if $event_logging 
-then
-  LOGS_DIR=/ramdisk
-  for log_comp in audio_preproc process.!override.!window_0 process.!override.!chunk_energy_0 4mic
-  do
-    ADD_OPTS="$ADD_OPTS -x ${log_comp}.!log_file=$LOGS_DIR/${log_comp}.log -x ${log_comp}.verbose=true"
-  done
-fi
-
-if $mic_audio_recording 
-then
-  echo 
-else
-  ADD_OPTS="$ADD_OPTS -x audio_writer_allchan=#audio_writer_allchan"
-fi
-
-REC_DEVNAME=$(arecord -L | grep -B 1 "seeed-4mic-voicecard" | grep "^hw")
-REC_DEVIDX=$(cat /proc/asound/cards | grep "seeed-4mic-voicecard" | head -1 | awk '{print $1;}')
-ADD_OPTS="$ADD_OPTS -x 4mic.soundcard_identifier=$REC_DEVNAME "
-
-if ! grep -q Microsoft /proc/version ;
-then
-  CMD_PREFIX="nice -n -20 "
-  for i in $(seq 0 3)
-  do 
-    sudo sh -c "echo performance > /sys/devices/system/cpu/cpu${i}/cpufreq/scaling_governor"
-  done
-fi
-
-debug=false
-if $debug
-then
-  CMD_PREFIX="$CMD_PREFIX gdb --args "
-fi
-
-$CMD_PREFIX godec -q -x "global_opts.SAMPLE_RATE=$SAMPLE_RATE" -x "global_opts.CAPTURE_CHUNK_SIZE=$CAPTURE_CHUNK_SIZE" -x "global_opts.CORRELATION_WINDOW_STEP_SIZE_MS=$CORRELATION_WINDOW_STEP_SIZE_MS" -x "global_opts.CORRELATION_WINDOW_SIZE_MS=$CORRELATION_WINDOW_SIZE_MS" -x "global_opts.OUTPUT_MODE=online" $ADD_OPTS online.json
-
-
+python3 run.py
