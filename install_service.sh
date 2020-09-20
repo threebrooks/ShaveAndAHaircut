1051c9e13fe88a4b1efcf007f6ca1f5753376482
Ralf Meermeier <ralf.meermeier@gmail.com>
2020-09-20 03:06:19 +0100

    Bla

--- install_service.sh
+++ install_service.sh
@@ -1,2 +1,22 @@
-sudo cp ShaveAndAHaircut.service /lib/systemd/system/
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "[Unit]
+Description=Shave and a haircut
+After=network-online.target
+
+[Service]
+ExecStart=/bin/bash $DIR/run.sh
+WorkingDirectory=$DIR
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=pi
+
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/ShaveAndAHaircut.service 
+
+sudo systemctl enable ShaveAndAHaircut.service
 sudo systemctl start ShaveAndAHaircut.service
+
